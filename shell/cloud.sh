a6dc0e92cefa8b19b8b649ec372963ab991e3cdb
tianyongping <tianyp@hxwisec.com>
2017-08-25 16:39:02 +0800

    服务器操作脚本提交

--- shell/cloud.sh
+++ shell/cloud.sh
@@ -1,4 +1,4 @@
-# 以下Spring Cloud项目启动脚本
+# 以下Spring Cloud项目启动脚本开始
 surl="http://192.168.100.137:8101/info"
 sjar="/home/SpringCloudService/deploy/registry-center-1.0.0.jar"
 stips="注册中心"
@@ -22,6 +22,7 @@ done
 
 echo ^Z
 echo -e "\033[35mStarted $stips successfully($tcount s). \033[0m"
+# 以下Spring Cloud项目启动脚本结束
 
 # 通过端口停止服务，例端口8101
 kill `lsof -t -i:8101`
